a04c49169df51b0513a13ba805b5ce4f7ba191e0
gevorgav <gevqwertyuiop@mail.ru>
2022-07-16 01:31:21 +0400

    Update index.php

--- index.php
+++ index.php
@@ -1,20 +1,14 @@
 <!DOCTYPE html>
 <!-- saved from url=(0160)https://dev-api.interpopcomics.com/dev/client/comics/ae8d5ac0fdda11ecadf41781e56fb373/share?fbclid=IwAR1mZI0ikjKl1B8-iL6Jg308Laho88wEvRPYAyMxak9FsyjbJGj2f3MWjNg -->
-<html lang="en" style="height: 100%; background: black" data-lt-installed="true"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-            <meta name="twitter:card" content="summary_large_image">
-            <meta name="twitter:image" content="https://s3.amazonaws.com/ipop-com-api-dev-comics-assets/ae8d5ac0fdda11ecadf41781e56fb373/pages/0/bdd259f0-fdda-11ec-be45-4d7365da49e1-0.jpg">
-            <meta property="og:image" content="https://s3.amazonaws.com/ipop-com-api-dev-comics-assets/ae8d5ac0fdda11ecadf41781e56fb373/pages/0/bdd259f0-fdda-11ec-be45-4d7365da49e1-0.jpg">
-            <meta property="og:url" content="https://dev.interpopcomics.com/comics/ae8d5ac0fdda11ecadf41781e56fb373/details"/>
-            <meta property="og:title" content="The Nine (Colleen Doran Cover)">
-            <meta property="og:type" content="article"/>
-            <meta property="og:description" content="&lt;p&gt;Follow the Crime Blotter in the pages of The Nine #0 as he investigates the cold case behind the disappearance of the villain Phaeton. What secrets will Crime Blotter dig up? Written by Will Pfeifer (Teen Titans) and drawn by Steve Ellis with cover by Colleen Doran. Welcome to the world of the Emergents!&lt;/p&gt;">
-            <meta http-equiv="Content-Security-Policy" content="default-src *;img-src * &#39;self&#39; 
-                                                                data: https:; script-src &#39;self&#39;
-                                                                &#39;unsafe-inline&#39; &#39;unsafe-eval&#39; *; 
-                                                                style-src  &#39;self&#39; &#39;unsafe-inline&#39; *">
+<html lang="en" style="height: 100%; background: black" data-lt-installed="true">
+<head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+            
           </head>
           <body>
-<div class="fb-share-button" data-href="https://dev-api.interpopcomics.com/dev/client/comics/ae8d5ac0fdda11ecadf41781e56fb373/share?fbclid=IwAR3ci-VJAIHA2azHD5eXMtb0pfzS4uJEa4bWFItC_Gxk3ENYsGvJNAD_3lw" 
-     data-layout="button_count" data-size="small"><a target="_blank"
-     href="https://www.facebook.com/sharer/sharer.php?u=https://dev-api.interpopcomics.com/dev/client/comics/ae8d5ac0fdda11ecadf41781e56fb373/share?fbclid=IwAR3ci-VJAIHA2azHD5eXMtb0pfzS4uJEa4bWFItC_Gxk3ENYsGvJNAD_3lw;src=sdkpreparse" class="fb-xfbml-parse-ignore">Share</a></div>
-          </body></html>
+<div>
+                      <h1>The Nine (Adriana Melo Cover)</h1>
+<img _ngcontent-jwo-c103="" class="poster" src="https://s3.amazonaws.com/ipop-com-api-dev-comics-assets/85b60840fdda11ecad530b6d14f56bf9/pages/0/95499ca0-fdda-11ec-bd1b-794cbd24fb16-0.jpg" alt="The Nine (Adriana Melo Cover)">
+            <p>Follow the Crime Blotter in the pages of The Nine #0 as he investigates the cold case behind the disappearance of the villain Phaeton. What secrets will Crime Blotter dig up? Written by Will Pfeifer (Teen Titans) and drawn by Steve Ellis with cover by Adriana Melo. Welcome to the world of the Emergents!</p>
+                      </div>
+          </body>
+</html>
