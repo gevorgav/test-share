2a766dec4de014fa924b38ec99351e9e393528d5
gevorgav <gevqwertyuiop@mail.ru>
2022-07-16 00:37:08 +0400

    Update index.php

--- index.php
+++ index.php
@@ -4,8 +4,9 @@
             <meta name="twitter:card" content="summary_large_image">
             <meta name="twitter:image" content="https://s3.amazonaws.com/ipop-com-api-dev-comics-assets/ae8d5ac0fdda11ecadf41781e56fb373/pages/0/bdd259f0-fdda-11ec-be45-4d7365da49e1-0.jpg">
             <meta property="og:image" content="https://s3.amazonaws.com/ipop-com-api-dev-comics-assets/ae8d5ac0fdda11ecadf41781e56fb373/pages/0/bdd259f0-fdda-11ec-be45-4d7365da49e1-0.jpg">
-            <meta property="og:url" content="https://dev.interpopcomics.com/comics/ae8d5ac0fdda11ecadf41781e56fb373/details">
+            <meta property="og:url" content="https://dev.interpopcomics.com/comics/ae8d5ac0fdda11ecadf41781e56fb373/details"/>
             <meta property="og:title" content="The Nine (Colleen Doran Cover)">
+            <meta property="og:type" content="article"/>
             <meta property="og:description" content="&lt;p&gt;Follow the Crime Blotter in the pages of The Nine #0 as he investigates the cold case behind the disappearance of the villain Phaeton. What secrets will Crime Blotter dig up? Written by Will Pfeifer (Teen Titans) and drawn by Steve Ellis with cover by Colleen Doran. Welcome to the world of the Emergents!&lt;/p&gt;">
             <meta http-equiv="Content-Security-Policy" content="default-src *;img-src * &#39;self&#39; data: https:; script-src &#39;self&#39; &#39;unsafe-inline&#39; &#39;unsafe-eval&#39; *; style-src  &#39;self&#39; &#39;unsafe-inline&#39; *">
           </head>
