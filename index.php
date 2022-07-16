cd697e5e57fa22cf8b678fbc4ba8b11404dd87a0
gevorgav <gevqwertyuiop@mail.ru>
2022-07-17 02:10:39 +0400

    Update index.php

--- index.php
+++ index.php
@@ -10,5 +10,6 @@
 <img _ngcontent-jwo-c103="" class="poster" src="https://s3.amazonaws.com/ipop-com-api-dev-comics-assets/85b60840fdda11ecad530b6d14f56bf9/pages/0/95499ca0-fdda-11ec-bd1b-794cbd24fb16-0.jpg" alt="The Nine (Adriana Melo Cover)">
             <p>Follow the Crime Blotter in the pages of The Nine #0 as he investigates the cold case behind the disappearance of the villain Phaeton. What secrets will Crime Blotter dig up? Written by Will Pfeifer (Teen Titans) and drawn by Steve Ellis with cover by Adriana Melo. Welcome to the world of the Emergents!</p>
                       </div>
+                      <img _ngcontent-jwo-c103="" class="poster" src="https://s3.amazonaws.com/ipop-com-api-dev-comics-assets/2c452d10fde211eca2120ff4dae39f52/pages/0/3c16dbe0-fde2-11ec-b1e6-53cd596d96be-0.jpg">
           </body>
 </html>
